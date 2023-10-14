e3ece0a1922cf39a1a32bf6e0c63cf278fe7c991
demilei221 <demilei221@gmail.com>
2023-10-13 21:22:13 -0400

    header UI implementation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Header from "./header";
 
 function App() {
-  return <Header />;
+  return <Header attachment={"https://ih1.redbubble.net/image.751252540.8766/flat,750x,075,f-pad,750x1000,f8f8f8.jpg"}/>;
 }
 
 export default App;
--- my-app/src/header.js
+++ my-app/src/header.js
@@ -1,7 +1,54 @@
-export default function Header() {
+import React, { useEffect, useState } from "react";
+// import "./StickyHeader.css";
+import "./index.css"
+export default function Header({attachment}) {
+  const [isSticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="sticky-header">
-      <h1>scrapbook</h1>;
+    <div className={`sticky-header ${isSticky ? "sticky" : ""}`}>
+      <div className="header-content">
+
+        <div className="left-content">scrapbook</div>
+
+        <div className="right-content">
+          <img src={attachment}
+               alt="Right-aligned Image"
+               className="right-image" 
+          />
+        </div>
+
+      </div>
     </div>
   );
+
+
+
+  // return (
+  //   <div className="sticky-header">
+  //     <h1>scrapbook</h1>;
+  //   </div>
+  
+  // );
 }
+
+
+
+
+  
+
+   
