fbf0e45125a5616d094395b2ec4e22f6a5b90081
Uma <uma2005anand@gmail.com>
2023-10-14 03:12:58 -0400

    feat: basic sign in ui

--- my-app/src/loginPage/SignUpPanel.jsx
+++ my-app/src/loginPage/SignUpPanel.jsx
@@ -29,97 +29,95 @@ export default function SignUpPage() {
   }
   return (
     <>
-      <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          backgroundColor: "#CC00FF",
+          color: "white",
+        }}
+      >
+        <h1 style={{ fontSize: "4em" }}>scrapbook</h1>
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100vh",
-            backgroundColor: "#CC00FF",
-            color: "white",
+            width: "20%",
+            minWidth: "300px",
           }}
         >
-          <h1 style={{ fontSize: "4em" }}>scrapbook</h1>
-          <div
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h4 style={{ margin: "0px", fontSize: "18px" }}>Username</h4>
+            <textarea
+              id="usernameBox"
+              rows="1"
+              style={{
+                border: "none",
+                borderRadius: "5px",
+                resize: "none",
+                padding: "8px",
+                margin: "0",
+                boxSizing: "border-box",
+                fontSize: "18px",
+              }}
+            ></textarea>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h4 style={{ marginBottom: "0px", fontSize: "18px" }}>Password</h4>
+            <input
+              id="passwordBox"
+              type="password"
+              style={{
+                border: "none",
+                borderRadius: "5px",
+                padding: "8px",
+                margin: "0",
+                boxSizing: "border-box",
+                fontSize: "18px",
+              }}
+            />
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h4 style={{ margin: "0px", marginTop: "20px", fontSize: "18px" }}>
+              Confirm Password
+            </h4>
+            <input
+              id="passwordBox2"
+              type="password" // Use type="password" to hide the text
+              style={{
+                border: "none",
+                borderRadius: "5px",
+                padding: "8px",
+                margin: "0",
+                boxSizing: "border-box",
+                fontSize: "18px",
+              }}
+            />
+          </div>
+          <button
             style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "20%",
-              minWidth: "300px",
+              marginTop: "60px",
+              marginBottom: "30px",
+              width: "100%",
+              backgroundColor: "white",
+              color: "#CC00FF",
+              border: "none",
+              borderRadius: "25px",
+              padding: "16px 16px",
+              fontSize: "18px",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              submit();
             }}
           >
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <h4 style={{ margin: "0px", fontSize: "18px" }}>Username</h4>
-              <textarea
-                id="usernameBox"
-                rows="1"
-                style={{
-                  border: "none",
-                  borderRadius: "5px",
-                  resize: "none",
-                  padding: "8px",
-                  margin: "0",
-                  boxSizing: "border-box",
-                  fontSize: "18px",
-                }}
-              ></textarea>
-            </div>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <h4 style={{ marginBottom: "0px", fontSize: "18px" }}>
-                Password
-              </h4>
-              <input
-                id="passwordBox"
-                type="password"
-                style={{
-                  border: "none",
-                  borderRadius: "5px",
-                  padding: "8px",
-                  margin: "0",
-                  boxSizing: "border-box",
-                  fontSize: "18px",
-                }}
-              />
-            </div>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <h4 style={{ margin: "0px", fontSize: "18px" }}>
-                Confirm Password
-              </h4>
-              <textarea
-                id="passwordBox2"
-                rows="1"
-                style={{
-                  border: "none",
-                  borderRadius: "5px",
-                  resize: "none",
-                  padding: "8px",
-                  margin: "0",
-                  boxSizing: "border-box",
-                  fontSize: "18px",
-                }}
-              ></textarea>
-            </div>
-            <button
-              style={{
-                marginTop: "10px",
-                width: "100%",
-                backgroundColor: "white",
-                color: "#CC00FF",
-                border: "none",
-                borderRadius: "25px",
-                padding: "16px 16px",
-                fontSize: "18px",
-                cursor: "pointer",
-              }}
-              onClick={submit}
-            >
-              <b>Submit</b>
-            </button>
-          </div>
+            <b>Sign Up</b>
+          </button>
         </div>
-      </>
+      </div>
     </>
   );
 }
