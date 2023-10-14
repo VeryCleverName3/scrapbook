c86ab606d151ffb0dab96813921afde69363670c
Uma <uma2005anand@gmail.com>
2023-10-14 03:04:07 -0400

    feat: login full ui

--- my-app/src/loginPage/SignUpPanel.jsx
+++ my-app/src/loginPage/SignUpPanel.jsx
@@ -1,40 +1,125 @@
-export default function SignUpPage(){
-    async function submit(){
-        let username = document.getElementById("usernameBox").value;
-        let password = document.getElementById("passwordBox").value;
-        let pass2 = document.getElementById("passwordBox2").value;
-        if(password == pass2){
-            let res = await fetch("http://localhost:8080/makeUser", {
-                method: "POST",
-                body: JSON.stringify({
-                    username: username,
-                    password: password
-                }),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            });
+export default function SignUpPage() {
+  async function submit() {
+    let username = document.getElementById("usernameBox").value;
+    let password = document.getElementById("passwordBox").value;
+    let pass2 = document.getElementById("passwordBox2").value;
+    if (password == pass2) {
+      let res = await fetch("http://localhost:8080/makeUser", {
+        method: "POST",
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
 
-            let result = await res.text();
+      let result = await res.text();
 
-            if(result == "good"){
-                localStorage.username = username;
-            } else {
-                localStorage.username = "";
-                console.log(result);
-            }
-        } else {
-            console.log("passwords don't match!");
-        }
+      if (result == "good") {
+        localStorage.username = username;
+      } else {
+        localStorage.username = "";
+        console.log(result);
+      }
+    } else {
+      console.log("passwords don't match!");
     }
-    return (<>
-        Username:
-        <textarea id="usernameBox"></textarea>
-        Password:
-        <textarea id="passwordBox"></textarea>
-        Confirm Password:
-        <textarea id="passwordBox2"></textarea>
-
-        <button onClick={submit}>Submit</button>
-    </>);
+  }
+  return (
+    <>
+      <>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            backgroundColor: "#CC00FF",
+            color: "white",
+          }}
+        >
+          <h1 style={{ fontSize: "4em" }}>scrapbook</h1>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "20%",
+              minWidth: "300px",
+            }}
+          >
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <h4 style={{ margin: "0px", fontSize: "18px" }}>Username</h4>
+              <textarea
+                id="usernameBox"
+                rows="1"
+                style={{
+                  border: "none",
+                  borderRadius: "5px",
+                  resize: "none",
+                  padding: "8px",
+                  margin: "0",
+                  boxSizing: "border-box",
+                  fontSize: "18px",
+                }}
+              ></textarea>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <h4 style={{ marginBottom: "0px", fontSize: "18px" }}>
+                Password
+              </h4>
+              <input
+                id="passwordBox"
+                type="password"
+                style={{
+                  border: "none",
+                  borderRadius: "5px",
+                  padding: "8px",
+                  margin: "0",
+                  boxSizing: "border-box",
+                  fontSize: "18px",
+                }}
+              />
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <h4 style={{ margin: "0px", fontSize: "18px" }}>
+                Confirm Password
+              </h4>
+              <textarea
+                id="passwordBox2"
+                rows="1"
+                style={{
+                  border: "none",
+                  borderRadius: "5px",
+                  resize: "none",
+                  padding: "8px",
+                  margin: "0",
+                  boxSizing: "border-box",
+                  fontSize: "18px",
+                }}
+              ></textarea>
+            </div>
+            <button
+              style={{
+                marginTop: "10px",
+                width: "100%",
+                backgroundColor: "white",
+                color: "#CC00FF",
+                border: "none",
+                borderRadius: "25px",
+                padding: "16px 16px",
+                fontSize: "18px",
+                cursor: "pointer",
+              }}
+              onClick={submit}
+            >
+              <b>Submit</b>
+            </button>
+          </div>
+        </div>
+      </>
+    </>
+  );
 }
