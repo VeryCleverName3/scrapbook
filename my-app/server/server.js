a8c7c580647a7db037e58e9df90dd0dfeb25a3c2
VeryCleverName3 <jsemrad6@gatech.edu>
2023-10-13 23:13:56 -0400

    🦕

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -1,64 +1,98 @@
 let express = require("express");
-
 let app = express();
-
 let multer = require("multer");
-
-/*
-POST request to make a post
-
-*/
-
-/*
-posts need: title, content, images, likes, dateUploaded, includedUsers
-*/
+let fs = require("fs");
 
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/meerkatDB`));
+app.use(express.static(`${__dirname}/meerkatDB/images`));
 
 let posts = {};
 let users = {};
+users = JSON.parse(fs.readFileSync(`${__dirname}/meerkatDB/text/users.JSON`));
+posts = JSON.parse(fs.readFileSync(`${__dirname}/meerkatDB/text/posts.JSON`));
 let numPosts = 0;
+
+function updateDatabase(){
+    fs.writeFileSync(`${__dirname}/meerkatDB/text/users.JSON`, JSON.stringify(users));
+    fs.writeFileSync(`${__dirname}/meerkatDB/text/posts.JSON`, JSON.stringify(posts));
+}
 
 //testing, not for anything
 app.get("/", (req, res) => {
     res.send(`
-<form action="/post" method="POST" enctype="multipart/form-data">
-  <label for="fname">Title:</label><br>
-  <input type="text" id="fname" name="fname"><br>
-  <label for="lname">Content:</label><br>
-  <input type="text" id="lname" name="lname">
-  <label for="images">Pictures:</label><br>
-  <input type="file" id="images" name="images" multiple>
-  <input type="submit" value="Submit">
-</form>
-`);
+        <form action="/post" method="POST" enctype="multipart/form-data">
+        <label for="fname">Title:</label><br>
+        <input type="text" id="fname" name="title"><br>
+        <label for="lname">Content:</label><br>
+        <input type="text" id="lname" name="content"><br>
+        <label for="pictures">Pictures:</label><br>
+        <input type="file" id="pictures" name="images" multiple><br>
+        <input type="hidden" name="userCookie" value="bobama7"><br>
+        <label for="otherusers">Other Users in Photo:</label>
+        <input type="text" id="otherUsers" name="includedUsers">
+        <input type="submit" value="Submit">
+        </form>
+    `);
 });
 
-let postingForm = multer({dest:"meerkatDB/images/"});
+app.post("/makeUser", (req, res) => {
+    let userData = req.body;
+
+    if(users[userData.username] == undefined){
+        users[userData.username] =  {name: userData.username, password: userData.password, profilePicUrl: "", posts: []}; 
+        res.send("ok");
+    } else {
+        res.send("username already taken!!");
+    }
+});
+
+app.post("/login", (req, res) => {
+    let data = req.body;
+    if(users[data.username].password == data.password){
+        res.send("succesfully logged in");
+    } else {
+        res.send("bad login, maybe incorrect password but who knows");
+    }
+});
+
+let postingForm = multer({dest:"meerkatDB/images/images"});
+
+/*
+post has inputs of names:
+title: title of post
+content: text content of post
+images: list of images added in the post
+includedUsers: other users included in the post
+userCookie: username of the poster
+
+*/
+
 //where user creates a post
 app.post("/post", postingForm.array("images", 5), (req, res, next) => {
-    let photo = req.files;
+    let photos = req.files;
     let text = req.body;
-    let includedUsers = text.includedUsers;
-    photo.url = `images/${photo.filename}`;
+    let includedUsers = text.includedUsers.split(","); //includedUsers is a comma separated list
+    includedUsers.push(text.userCookie);
 
-    let post = {text, photo};
+    for(let photo of photos){
+        photo.url = `images/${photo.filename}`;
+    }
+
+    let post = {text, photos};
 
     console.log(post);
     post.id = numPosts++;
     posts[post.id] = post;
+
+    for(let i of includedUsers){
+        if(users[i] != undefined){
+            users[i].posts.push(post);
+        }
+    }
+
+    updateDatabase();
     res.send("yo!");
-
-
-    /*
-    let post = req.body;
-    let id = numPosts++;
-    post.id = id;
-    posts[id] = post;
-    res.send("yo");
-    */
 });
 
 app.get("/getPost/:id", (req, res) => {
@@ -66,7 +100,12 @@ app.get("/getPost/:id", (req, res) => {
 });
 
 app.get("/postIdsFor/:userId", (req, res) => {
+    let postIds = [];
+    for(let i of users[req.params.userId].posts){
+        postIds.push(i.id);
+    }
 
+    res.send(JSON.stringify(postIds));
 });
 
 app.listen(8080);
